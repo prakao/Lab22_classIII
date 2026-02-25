d8d8aba277fe93ade2ad5f8b0ec3b810bf6960ae
prakao <kpkrisrin@gmail.com>
2026-02-26 00:57:04 +0700

    Lab22_finished

--- lab22.cpp
+++ lab22.cpp
@@ -24,8 +24,77 @@ ComplexNumber ComplexNumber::operator+(const ComplexNumber &c){
 	return ComplexNumber(real+c.real,imag+c.imag);
 }
 
+ComplexNumber operator+(double s,const ComplexNumber &c){
+	return ComplexNumber(s+c.real,c.imag);
+}
+
 ComplexNumber ComplexNumber::operator-(const ComplexNumber &c){
 	return ComplexNumber(real-c.real,imag-c.imag);
+}
+
+ComplexNumber operator-(double s,const ComplexNumber &c){
+	return ComplexNumber(s-c.real,-c.imag);
+}
+
+ComplexNumber ComplexNumber::operator*(const ComplexNumber &c){
+	return ComplexNumber(real*c.real-imag*c.imag,real*c.imag+imag*c.real);
+}
+
+ComplexNumber operator*(double s,const ComplexNumber &c){
+	return ComplexNumber(s*c.real,s*c.imag);
+}
+
+ComplexNumber ComplexNumber::operator/(const ComplexNumber&c){
+	double d=pow(c.real,2)+pow(c.imag,2);
+	return ComplexNumber((real*c.real+imag*c.imag)/d,(imag*c.real-real*c.imag)/d);
+}
+
+ComplexNumber operator/(double s,const ComplexNumber&c){
+	double d=pow(c.real,2)+pow(c.imag,2);
+	return ComplexNumber((s*c.real)/d,(-s*c.imag)/d);
+}
+
+bool ComplexNumber::operator==(const ComplexNumber&c){
+	if(real==c.real&&imag==c.imag){
+		return true;
+	}else{
+		return false;
+	}
+}
+
+bool operator==(double s, const ComplexNumber&c){
+	if(s==c.real&&c.imag==0){
+		return true;
+	}else{
+		return false;
+	}
+}
+
+double ComplexNumber::abs(){
+	return sqrt(pow(real,2)+pow(imag,2));
+}
+
+double ComplexNumber::angle(){
+	return atan2(imag,real)*(180/M_PI);
+}
+
+ostream & operator<<(ostream &os,const ComplexNumber &c){
+	if(c.real==0&&c.imag==0){
+		os<<0;
+	}else if(c.real==0){
+		os<<c.imag<<"i";
+	}else if(c.imag==0){
+		os<<c.real;
+	}else {
+		os<<c.real;
+		if(c.imag>0){
+			os<<"+"<<c.imag<<"i";
+		}else{
+			os<<c.imag<<"i";
+		}
+	}
+	return os;
+
 }
 
 //Write your code here
